ad25661b4dffc224d179987538f7e27fc4669e35
Bataycan21 <158477898+Bataycan21@users.noreply.github.com>
2025-12-12 21:17:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function checkEmail() {
   const email = document.getElementById('emailInput').value;
-  const isValid = email.includes('@');
+  const isValid = email.includes('@gmail.com');
   document.getElementById('emailOutput').textContent = `Output: ${isValid ? 'Valid' : 'Invalid'}`;
 }
